43448b7b5765a8e8ed3142fc12fc96dce309a018
kubat <kubat.aiupov@gmail.com>
2024-06-17 21:45:08 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import GalleryPage from "./pages/GalleryPage/GalleryPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: true
-    }
-  }
+      refetchOnWindowFocus: true,
+    },
+  },
 });
 
 function App() {
   return (
     <>
-      <LandingPage />
       <QueryClientProvider client={queryClient}>
-          <GalleryPage />
+        <LandingPage />
+        {/* <Routes>
+          <Route path="/galleryPage" element={<GalleryPage />} />
+        </Routes> */}
       </QueryClientProvider>
     </>
   );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./LandingPage.module.css";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <main className={styles.container}>
       <header className={styles.header}>
         <h1 className={styles.title}>MARS-IMAGE-GALLERY</h1>
-        <button className={styles.btn}>go to the gallery</button>
+        <Link to="/galleryPage" className={styles.btn}>
+          go to the gallery
+        </Link>
       </header>
       <div className={styles.bottom}></div>
     </main>
