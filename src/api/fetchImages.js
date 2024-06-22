d751276024597f248b754e2e7a76d69f77ca398e
kubat <kubat.aiupov@gmail.com>
2024-06-22 18:31:48 +0200

    add fix url in api

--- src/api/fetchImages.js
+++ src/api/fetchImages.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function fetchImages() {
   const apiKey = import.meta.env.VITE_API_KEY;
-  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=10&api_key=${apiKey}`;
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`;
   return axios
     .get(url, {
       headers: {
